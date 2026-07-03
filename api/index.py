5b5c428da2b437eccfe773efebf147096fac94f4
Sivasathwik <sivasathwik@users.noreply.github.com>
2026-07-03 23:54:38 +0530

    Run database migrations on Vercel startup

--- api/index.py
+++ api/index.py
@@ -7,7 +7,13 @@ sys.path.append(str(BASE_DIR))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coffeshop.settings")
 
+from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
+
+if os.environ.get("VERCEL"):
+    call_command("migrate", interactive=False, verbosity=0)
+    call_command("add_sample_menu", verbosity=0)
+
 app = application
