acb468f78821c46d1c43865857275f0014f0fb6b
Sivasathwik <sivasathwik@users.noreply.github.com>
2026-07-03 23:49:56 +0530

    Expose Vercel WSGI application alias

--- api/index.py
+++ api/index.py
@@ -9,4 +9,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coffeshop.settings")
 
 from django.core.wsgi import get_wsgi_application
 
-app = get_wsgi_application()
+application = get_wsgi_application()
+app = application
